837c9ec9beaeac3c4257471a05ca42f3c153d8bb
farasu <mohammadjanfarasu@gmail.com>
2023-03-30 09:26:45 +0430

    add css style

--- src/component/form/ExpenseForm.js
+++ src/component/form/ExpenseForm.js
@@ -1,19 +1,21 @@
+import style from './ExpenseForm.module.css';
+
 function ExpenseForm(){
     return(
-      <div>
-        <div>
+      <div className={style['form-control']}>
+        <div className={style['form-element']}>
           <label>Title</label>
           <input type="text" />
         </div>
-        <div>
+        <div className={style['form-element']}>
           <label>Name</label>
           <input type="text" />
         </div>
-        <div>
+        <div className={style['form-element']}>
           <label>Price</label>
           <input type="text" />
         </div>
-        <div>
+        <div className={style['form-element']}>
             <button type="submit">Submit</button>
         </div>
       </div>
